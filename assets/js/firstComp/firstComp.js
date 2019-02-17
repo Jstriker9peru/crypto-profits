967e7d2e84484033844a5f86fb5fdce56c01caa7
Jesp <jstriker9peru@gmail.com>
2019-02-17 11:30:47 -0500

    Added bootstrap grid layout

--- assets/js/firstComp/firstComp.js
+++ assets/js/firstComp/firstComp.js
@@ -13,6 +13,11 @@ class Layout extends Component {
     return (
       <div className='home'>
         This is the home page
+        <div className="col-md-offset-4 col-md-6">
+          <div className="box1">
+
+          </div>
+        </div>
       </div>
     )
   }
